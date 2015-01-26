c13f00f2352ccdbfe506744fc6bf9d2c9c903909
4johnny <4johnny@users.noreply.github.com>
2015-01-26 10:32:28 -0800

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Johnny on 2015-01-26.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Johnny on 2015-01-26.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
